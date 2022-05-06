239453b18f48472a42bd601d376565b994214e13
Ahmed Al Ashhab <96653465+dudeofmann@users.noreply.github.com>
2022-05-06 12:16:34 +0300

    fixed

--- js/script.js
+++ js/script.js
@@ -17,7 +17,6 @@ btnNavEl.addEventListener("click", function () {
 // Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
-console.log(allLinks);
 
 allLinks.forEach(function (link) {
 	link.addEventListener("click", function (e) {
@@ -54,7 +53,6 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
 	function (entries) {
 		const ent = entries[0];
-		console.log(ent);
 
 		if (ent.isIntersecting === false) {
 			document.body.classList.add("sticky");
@@ -86,7 +84,6 @@ function checkFlexGap() {
 	document.body.appendChild(flex);
 	var isSupported = flex.scrollHeight === 1;
 	flex.parentNode.removeChild(flex);
-	console.log(isSupported);
 
 	if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
